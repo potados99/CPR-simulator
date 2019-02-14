9cfad6c76eb00784d871288a904c0d4a3884a47b
potados <potados99@gmail.com>
2019-02-14 10:43:11 +0900

    kind.

--- wrapper.cpp
+++ wrapper.cpp
@@ -9,27 +9,27 @@ Display _BPMDisp(5, 3); /* monitor에 연결할 디스플레이. */
 PulseMonitor monitor; /* 현재 압박 빈도를 계산하고 표시하기 위해 사용. */
 
 void setup() {
-  // 센서 설정
+  // 센서를 설정합니다.
   _pushSensor.setSensitivity(90);
   _pushSensor.setSmoothness(20);
   _pushSensor.setScale(100);
   _pushSensor.setOptions(F_SENSITIVITY | F_SMOOTHNESS | F_SCALE);
 
-  // monitor에 센서와 디스플레이 연결
+  // monitor에 센서와 디스플레이를 연결합니다.
   monitor.attachSensor(&_pushSensor);
   monitor.attachDisplay(&_BPMDisp);
 
-  // monitor 설정
+  // monitor를 설정합니다.
   monitor.setThreshold(70);
   monitor.setRefreshCycle(1000);
 
-  // monitor 시작
+  // monitor를 시작합니다.
   monitor.start();
 }
 
 void loop() {
   monitor.loop();
 
-  // 현재 압력 표시.
+  // 현재 압력을 표시합니다.
   presDisp.show(monitor.getPressure() / 10, -1); /* -1: 소수점 없음. */
 }
